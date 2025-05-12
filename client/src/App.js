c57e78ddcdf021960d6ef3a7ec3a0a245a1942a2
Annie Huynh <maiannie03@icloud.com>
2025-05-11 23:53:03 -0400

    Restructure project folders

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Aesthetic Study Companion</h1>
+      <p>This is a simple web application designed to help you study effectively while keeping you engaged through aesthetics.</p>
+    </div>  
+    );
+}
+
+export default App;
